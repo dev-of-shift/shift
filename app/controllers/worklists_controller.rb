3c6adbeecd2f2481221db48815ce106b262e4fb4
breastroke423 <breastroke423@gmail.com>
2020-12-02 07:13:18 +0000

    update worklist

--- app/controllers/worklists_controller.rb
+++ app/controllers/worklists_controller.rb
@@ -1,2 +1,25 @@
 class WorklistsController < ApplicationController
+  def create
+    @worklist = Worklist.new(worklist_params)
+    @worklist.save
+    redirect_to places_path
+  end
+
+  def update
+    @worklist = Worklist.find(params[:id])
+    @worklist.update(worklist_params)
+    redirect_to places_path
+  end
+
+  def destroy
+    @worklist = Worklist.find(params[:id])
+    @worklist.destroy
+    redirect_to places_path
+  end
+
+
+  private
+  def worklist_params
+    params.require(:worklist).permit(:list_num, :list_day, :list_hour, :list_minutes)
+  end
 end
