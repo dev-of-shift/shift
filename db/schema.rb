d17fa9b95d97f3fc15b1910c577015b13df285ec
akirahashibami <hashibami2207@gmail.com>
2020-12-16 08:11:48 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2020_12_02_061247) do
 
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
