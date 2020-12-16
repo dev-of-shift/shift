0bb7f12c4df156fac442325891dcba06249a485d
akirahashibami <hashibami2207@gmail.com>
2020-12-16 08:35:12 +0000

    rr

--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -1,7 +1,14 @@
 class WorkdaysController < ApplicationController
 
   def month
-    # code
+    # 現在の曜日と年月日
+    date_now = Date.current
+
+    @weeks = ["日", "月", "火", "水", "木", "金", "土"]
+
+    firstDay = date_now.beginning_of_month
+    firstDayIndex = firstDay.wday
+    @calender = Array.new(35){|i|i}.map{ |i| firstDay + (i-firstDayIndex)}
   end
 
   def week
