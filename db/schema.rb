b141ba70d15464de9ebcb738320465b533ff7d6f
breastroke423 <breastroke423@gmail.com>
2020-12-19 06:01:32 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_12_19_035240) do
-=======
-ActiveRecord::Schema.define(version: 2020_12_02_061247) do
->>>>>>> 2c759ba... make seeds
 
+ActiveRecord::Schema.define(version: 2020_12_02_061247) do
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
